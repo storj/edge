161df2424aaa40afd45afa39ab285d76a3f3427a
Jeff Wendling <leterip@gmail.com>
2020-12-14 18:05:24 -0500

    auth/sqlauth: fix errors with newest staticcheck
    Change-Id: I5e8c7c94a537784dc62964e361d189fd280c8c35

--- auth/sqlauth/gen.sh
+++ auth/sqlauth/gen.sh
@@ -3,4 +3,11 @@
 set -e
 
 dbx golang -p sqlauth -d pgxcockroach -d sqlite3 sqlauth.dbx .
-( echo '//lint:file-ignore * generated file'; cat sqlauth.dbx.go ) > sqlauth.dbx.go.tmp && mv sqlauth.dbx.go.tmp sqlauth.dbx.go
+
+(
+    echo '//lint:file-ignore U1000 generated file'
+    echo '//lint:file-ignore ST1012 generated file'
+    cat sqlauth.dbx.go
+) > sqlauth.dbx.go.tmp
+
+mv sqlauth.dbx.go.tmp sqlauth.dbx.go
