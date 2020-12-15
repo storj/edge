ab57a727c57a1925393a9c0963636d9b55a903b1
Stefan Benten <mail@stefan-benten.de>
2020-12-15 03:12:16 +0100

    auth/sqlauth: update lint ignore for staticcheck 2020.2 (#6)

--- auth/sqlauth/gen.sh
+++ auth/sqlauth/gen.sh
@@ -3,11 +3,4 @@
 set -e
 
 dbx golang -p sqlauth -d pgxcockroach -d sqlite3 sqlauth.dbx .
-
-(
-    echo '//lint:file-ignore U1000 generated file'
-    echo '//lint:file-ignore ST1012 generated file'
-    cat sqlauth.dbx.go
-) > sqlauth.dbx.go.tmp
-
-mv sqlauth.dbx.go.tmp sqlauth.dbx.go
+( printf '%s\n' '//lint:file-ignore U1000,ST1012 generated file'; cat sqlauth.dbx.go ) > sqlauth.dbx.go.tmp && mv sqlauth.dbx.go.tmp sqlauth.dbx.go
