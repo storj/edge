a64b0cd8933fbc409c02c46e301f5b2e70002c23
Isaac Hess <isaachess@gmail.com>
2021-01-14 16:07:48 -0700

    docker-local: Print out convenient env variables
    For convenience, when the gateway has registered the access with the
    authservice, it prints the response out in one line with the env
    variables so local devs can easily add them to commands.
    
    Change-Id: Idb00d94950e9b63ebca92205a0985a4da02b3c7c

--- docker-local/start-gateway-mt.sh
+++ docker-local/start-gateway-mt.sh
@@ -13,10 +13,24 @@ until uplink ls --access "$(storj-sim network env GATEWAY_0_ACCESS --config-dir 
 done
 
 access=$(storj-sim network env GATEWAY_0_ACCESS --config-dir /config/local-network)
+keys=$(uplink access register "${access}" --auth-service http://authservice:8000)
+access_key_id=$(echo "${keys}" | grep "Access Key ID" | rev | cut -d " " -f1 | rev)
+secret_key_id=$(echo "${keys}" | grep "Secret Key" | rev | cut -d " " -f1 | rev)
+
+if [ -z "$access_key_id" ]; then
+    echo "Failed to get access_key_id"
+    exit 1
+fi
+if [ -z "$secret_key_id" ]; then
+    echo "Failed to get access_key_id"
+    exit 1
+fi
 echo "==================================================================="
 echo "Access ${access}"
 echo "==================================================================="
-uplink access register "${access}" --auth-service http://authservice:8000
+echo "Access Key ID: $access_key_id"
+echo "Secret Key ID: $secret_key_id"
+echo "AWS_ACCESS_KEY_ID=$access_key_id AWS_SECRET_ACCESS_KEY=$secret_key_id"
 echo "==================================================================="
 
 gateway-mt run --server.address="0.0.0.0:7777" --auth-token=super-secret --auth-url=http://authservice:8000 --domain-name=gateway.local
