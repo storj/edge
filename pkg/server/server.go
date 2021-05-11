storj/edge

commit 80a856055cc4b7028932a96b720a44ca48cc2713

gateway-mt:  Merge experimental code
Caleb had an 'experimental' directory of code in Gateway-MT, and with
new employees starting, it seems prudent to merge that code into the
active codebase to not confuse people.

Overall, Caleb's additions used a lot of extra structs that don't offer
much benefit, and they stored a lot of things in the context.  I've
attempted to merge these things into the Server struct, so most of the
changes appear there.

In addition, we've begun to realize that its difficult to track what
endpoints Gateway-MT supports.  I begun to break these endpoints into
their own files for clarity and shorter files.  This leaves the main
server.go file with helper functions that more clearly show what it
does, while pushing the endpoint implementations into their own files.
The remaining functions to be ported away from Minio now have a
unimplemented.go file to see clearly what remains un-ported.

Unfortuntely, these functions are not yet operable on their own.  We
need to be able to get authorization working correctly first, as the
Server.GetUplinkProject we extracted from 'experimental' tries to find
the auth credentials in the context, but currently nothing saves these
credentials to the context.  That will come in another PR.

Error handling and mapping has been the real problem exposed by the
Gateway-MT logs.  I've created an errors.go file where all server
errors can be mapped to S3 errors.

Finally, I've begun working toward unwinding the extent to which we've
forked Minio.  The hard fork of Minio wasn't a problem so much as the
very large sections of code we removed from it.  We've learned over
time that a lot of 3rd party tools called some of the endpoints we
removed in the Minio hard fork.  In addition, a large amount of our
additions to minio code were simply for exposing unexported methods.
I've added an expose.go file which uses the linkname feature / trick
to import methods from Minio without altering the Minio codebase.

pkg/server/server.go
@@ -12,39 +12,44 @@ import (
 
 	"github.com/gorilla/mux"
 	minio "github.com/storj/minio/cmd"
+	"github.com/storj/minio/pkg/storj/middleware/signature"
 	"github.com/zeebo/errs"
 	"go.uber.org/zap"
 	"golang.org/x/sync/errgroup"
 	"gopkg.in/webhelp.v1/whlog"
 
 	"storj.io/common/errs2"
+	"storj.io/common/rpc/rpcpool"
+	"storj.io/common/useragent"
 	"storj.io/gateway-mt/pkg/server/middleware"
+	"storj.io/private/version"
+	"storj.io/uplink"
+	"storj.io/uplink/private/transport"
 )
 
 var (
 	// Error is an error class for internal Multinode Dashboard http server error.
-	Error = errs.Class("S3 compatible server error")
+	Error = errs.Class("gateway")
 )
-
-// Config contains configuration for an S3 compatible http server.
-type Config struct {
-	Address     string
-	DomainNames []string
-}
 
 // Server represents an S3 compatible http server.
 type Server struct {
-	http     http.Server
-	listener net.Listener
-	log      *zap.Logger
+	http         http.Server
+	listener     net.Listener
+	log          *zap.Logger
+	Address      string
+	DomainNames  []string
+	RPCPool      *rpcpool.Pool
+	AuthClient   *AuthClient
+	UplinkConfig *uplink.Config
 }
 
 // New returns new instance of an S3 compatible http server.
-func New(listener net.Listener, log *zap.Logger, tlsConfig *tls.Config, config Config) *Server {
+func New(listener net.Listener, log *zap.Logger, tlsConfig *tls.Config, address string, domainNames []string) *Server {
 	r := mux.NewRouter()
 	r.SkipClean(true)
 
-	s := &Server{listener: listener, log: log, http: http.Server{Handler: r, Addr: config.Address}}
+	s := &Server{listener: listener, log: log, http: http.Server{Handler: r, Addr: address}}
 
 	if tlsConfig != nil {
 		s.listener = tls.NewListener(listener, tlsConfig)
@@ -54,11 +59,10 @@ func New(listener net.Listener, log *zap.Logger, tlsConfig *tls.Config, config C
 	publicServices := r.PathPrefix("/-/").Subrouter()
 	publicServices.HandleFunc("/health", s.healthCheck)
 
-	for _, domainName := range config.DomainNames {
+	for _, domainName := range domainNames {
 		pathStyle := r.Host(domainName).Subrouter()
 		s.AddRoutes(pathStyle, "/{bucket:.+}", "/{bucket:.+}/{key:.+}")
-		// this route was tested, but we have them commented out because they're currently not implemented
-		// pathStyle.HandleFunc("/", s.ListBuckets).Methods(http.MethodGet)
+		pathStyle.HandleFunc("/", s.ListBuckets).Methods(http.MethodGet)
 
 		virtualHostStyle := r.Host("{bucket:.+}." + domainName).Subrouter()
 		s.AddRoutes(virtualHostStyle, "/", "/{key:.+}")
@@ -145,137 +149,54 @@ func (s *Server) Close() error {
 	return Error.Wrap(s.http.Close())
 }
 
-// AbortMultipartUpload aborts a multipart upload.
-func (s *Server) AbortMultipartUpload(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("AbortMultipartUpload")
-}
-
-// CompleteMultipartUpload completes a multipart upload.
-func (s *Server) CompleteMultipartUpload(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("CompleteMultipartUpload")
-}
-
-// CopyObject copies and object.
-func (s *Server) CopyObject(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("CopyObject")
-}
-
-// CreateBucket creates a bucket.
-func (s *Server) CreateBucket(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("CreateBucket")
-}
-
-// CreateMultipartUpload creates a multipart upload.
-func (s *Server) CreateMultipartUpload(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("CreateMultipartUpload")
-}
-
-// DeleteBucket deletes a bucket.
-func (s *Server) DeleteBucket(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("DeleteBucket")
-}
-
-// DeleteBucketTagging deletes the tagging of a bucket.
-func (s *Server) DeleteBucketTagging(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("DeleteBucketTagging")
-}
-
-// DeleteObject deletes an object.
-func (s *Server) DeleteObject(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("DeleteObject")
-}
-
-// DeleteObjectTagging deletes the tagging of an object.
-func (s *Server) DeleteObjectTagging(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("DeleteObjectTagging")
-}
-
-// DeleteObjects deletes objects.
-func (s *Server) DeleteObjects(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("DeleteObjects")
-}
-
-// GetBucketTagging deletes the tagging of a bucket.
-func (s *Server) GetBucketTagging(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("GetBucketTagging")
-}
-
-// GetBucketVersioning returns the versioning state of a bucket.
-func (s *Server) GetBucketVersioning(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("GetBucketVersioning")
-	// todo:  consider if <Status>Suspended</Status> is better
-	_, err := w.Write([]byte(`<VersioningConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/"/>`))
+// WithProject handles opening and closing a project within a request handler.
+func (s *Server) WithProject(w http.ResponseWriter, r *http.Request, h func(context.Context, *uplink.Project) error) {
+	ctx := r.Context()
+	creds := signature.GetCredentials(ctx)
+	authAccess, err := s.AuthClient.GetAccess(ctx, creds.AccessKeyID)
 	if err != nil {
-		s.log.Error("GetBucketVersioning", zap.Error(err))
+		s.WriteError(ctx, w, err, r.URL)
+		return
+	}
+	accessGrant, err := uplink.ParseAccess(authAccess.AccessGrant)
+	if err != nil {
+		s.WriteError(ctx, w, err, r.URL)
+		return
+	}
+	uplinkConfig := s.UplinkConfig
+	uplinkConfig.UserAgent = getUserAgent(r.UserAgent())
+	err = transport.SetConnectionPool(ctx, s.UplinkConfig, s.RPCPool)
+	if err != nil {
+		s.WriteError(ctx, w, err, r.URL)
+		return
+	}
+
+	project, err := uplinkConfig.OpenProject(ctx, accessGrant)
+	if err != nil {
+		s.WriteError(ctx, w, err, r.URL)
+		return
+	}
+	defer func() {
+		if err := project.Close(); err != nil {
+			s.log.Warn("Failed to close project", zap.Error(err))
+		}
+	}()
+	err = h(ctx, project)
+	if err != nil {
+		s.WriteError(ctx, w, err, r.URL)
+		return
 	}
 }
 
-// GetObject returns an object.
-func (s *Server) GetObject(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("GetObject")
-}
+var gatewayUserAgent = "Gateway-MT/" + version.Build.Version.String()
 
-// GetObjectTagging returns the tagging of an object.
-func (s *Server) GetObjectTagging(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("GetObjectTagging")
-}
-
-// HeadBucket returns http headers about a bucket.
-func (s *Server) HeadBucket(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("HeadBucket")
-}
-
-// HeadObject returns http headers about an object.
-func (s *Server) HeadObject(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("HeadObject")
-}
-
-// ListBuckets returns a list of buckets.
-func (s *Server) ListBuckets(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("ListBuckets")
-}
-
-// ListMultipartUploads returns a list of multipart uploads.
-func (s *Server) ListMultipartUploads(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("ListMultipartUploads")
-}
-
-// ListObjects returns a list of objects.
-func (s *Server) ListObjects(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("ListObjects")
-}
-
-// ListObjectsV2 returns a list of objects.
-func (s *Server) ListObjectsV2(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("ListObjectsV2")
-}
-
-// ListParts returns a list of parts of a multipart upload.
-func (s *Server) ListParts(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("ListParts")
-}
-
-// PutBucketTagging adds tagging to a bucket.
-func (s *Server) PutBucketTagging(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("PutBucketTagging")
-}
-
-// PutObject uploads an objects.
-func (s *Server) PutObject(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("PutObject")
-}
-
-// PutObjectTagging adds tagging to an object.
-func (s *Server) PutObjectTagging(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("PutObjectTagging")
-}
-
-// UploadPart uploads part of a multipart upload.
-func (s *Server) UploadPart(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("UploadPart")
-}
-
-// UploadPartCopy copies part of a multipart upload to another object.
-func (s *Server) UploadPartCopy(w http.ResponseWriter, r *http.Request) {
-	s.log.Debug("UploadPartCopy")
+func getUserAgent(clientAgent string) string {
+	if clientAgent == "" {
+		return gatewayUserAgent
+	}
+	_, err := useragent.ParseEntries([]byte(clientAgent))
+	if err != nil {
+		return gatewayUserAgent
+	}
+	return gatewayUserAgent + " " + clientAgent
 }

cmd/gateway-mt/main.go
@@ -56,7 +56,7 @@ type Config struct {
 
 var (
 	// Error is the default gateway setup errs class.
-	Error = errs.Class("gateway setup error")
+	Error = errs.Class("gateway setup")
 	// rootCmd represents the base gateway command when called without any subcommands.
 	rootCmd = &cobra.Command{
 		Use:   "gateway",
@@ -160,7 +160,6 @@ func (flags GatewayFlags) Run(ctx context.Context, address string) (err error) {
 
 	// because existing configs contain most of these values, we don't have separate
 	// parameter bindings for the non-Minio server
-	serverConfig := server.Config{Address: address, DomainNames: strings.Split(runCfg.DomainName, ",")}
 	var tlsConfig *tls.Config
 	if !runCfg.InsecureDisableTLS {
 		tlsConfig, err = server.LoadTLSConfigFromDir(runCfg.CertDir)
@@ -168,7 +167,7 @@ func (flags GatewayFlags) Run(ctx context.Context, address string) (err error) {
 			return err
 		}
 	}
-	s3 := server.New(listener, zap.L(), tlsConfig, serverConfig)
+	s3 := server.New(listener, zap.L(), tlsConfig, address, strings.Split(runCfg.DomainName, ","))
 	runError := s3.Run(ctx)
 	closeError := s3.Close()
 	return errs.Combine(runError, closeError)

experimental/bucket.go
@@ -1,129 +1,0 @@
-// Copyright (C) 2021 Storj Labs, Inc.
-// See LICENSE for copying information.
-
-package main
-
-import (
-	"fmt"
-	"net/http"
-
-	"github.com/gorilla/mux"
-	"github.com/storj/gateway-mt/experimental/middleware/project"
-	"github.com/storj/minio/pkg/storj/model"
-)
-
-// ListBuckets https://docs.aws.amazon.com/AmazonS3/latest/API/API_ListBuckets.html
-func (h *Handler) ListBuckets(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("ListBuckets", r)
-
-	ctx := r.Context()
-
-	p := project.GetUplinkProject(ctx)
-
-	response := &model.ListBucketsResponse{}
-
-	buckets := []model.Bucket{}
-
-	bi := p.ListBuckets(ctx, nil)
-	for bi.Next() {
-		item := bi.Item()
-		bucket := model.Bucket{
-			Name:         item.Name,
-			CreationDate: model.ISO8601(item.Created),
-		}
-		buckets = append(buckets, bucket)
-	}
-	if err := bi.Err(); err != nil {
-		model.Error{
-			Err:    err,
-			Status: 500,
-		}.ServeHTTP(w, r)
-
-		return
-	}
-	response.Buckets.Buckets = buckets
-
-	body, err := model.Encode("application/xml", response)
-	if err != nil {
-		model.Error{
-			Err:    err,
-			Status: 500,
-		}.ServeHTTP(w, r)
-
-		return
-	}
-
-	w.Write(body)
-}
-
-// CreateBucket https://docs.aws.amazon.com/AmazonS3/latest/API/API_CreateBucket.html
-func (h *Handler) CreateBucket(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("CreateBucket", r)
-
-	ctx := r.Context()
-
-	p := project.GetUplinkProject(ctx)
-
-	bucket := mux.Vars(r)["bucket"]
-
-	_, err := p.CreateBucket(ctx, bucket)
-	if err != nil {
-		model.Error{
-			Err:    err,
-			Status: 500,
-		}.ServeHTTP(w, r)
-
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/xml")
-	w.WriteHeader(http.StatusCreated)
-}
-
-// HeadBucket https://docs.aws.amazon.com/AmazonS3/latest/API/API_HeadBucket.html
-func (h *Handler) HeadBucket(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("HeadBucket", r)
-
-	ctx := r.Context()
-
-	p := project.GetUplinkProject(ctx)
-
-	bucket := mux.Vars(r)["bucket"]
-
-	_, err := p.StatBucket(ctx, bucket)
-	if err != nil {
-		model.Error{
-			Err:    err,
-			Status: http.StatusNotFound,
-		}.ServeHTTP(w, r)
-
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/xml")
-	w.WriteHeader(http.StatusNoContent)
-}
-
-// DeleteBucket https://docs.aws.amazon.com/AmazonS3/latest/API/API_DeleteBucket.html
-func (h *Handler) DeleteBucket(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("DeleteBucket", r)
-
-	ctx := r.Context()
-
-	p := project.GetUplinkProject(ctx)
-
-	bucket := mux.Vars(r)["bucket"]
-
-	_, err := p.DeleteBucket(ctx, bucket)
-	if err != nil {
-		model.Error{
-			Err:    err,
-			Status: 500,
-		}.ServeHTTP(w, r)
-
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/xml")
-	w.WriteHeader(http.StatusNoContent)
-}

experimental/go.mod
@@ -1,18 +1,0 @@
-module github.com/storj/gateway-mt/experimental
-
-go 1.15
-
-require (
-	github.com/calebcase/tmpfile v1.0.2 // indirect
-	github.com/gorilla/mux v1.8.0
-	github.com/rs/cors v1.7.0
-	github.com/storj/minio/pkg/storj v0.0.0-20210303221741-b0adac24cc4e
-	github.com/zeebo/errs v1.2.2
-	go.uber.org/zap v1.16.0 // indirect
-	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20210305034016-7844c3c200c3 // indirect
-	storj.io/common v0.0.0-20210225132539-d54de2b98029
-	storj.io/drpc v0.0.17 // indirect
-	storj.io/uplink v1.4.5
-)

commit 6457cc0d9d3c1fda1bc1c7efdb2f7cd2a3c1dbb1

gateway-mt: remove listbucket custom handler
https://github.com/storj/gateway-mt/commit/80a856055cc4b7028932a96b720a44ca48cc2713
broke things by accidentally including an uncommented listbucket
handler.  We don't want that uncommented.

Change-Id: I3567490be7a221cfe7aaa63d0c12669f4772a996

pkg/server/server.go
@@ -64,7 +64,7 @@ func New(listener net.Listener, log *zap.Logger, tlsConfig *tls.Config, address 
 	for _, domainName := range domainNames {
 		pathStyle := r.Host(domainName).Subrouter()
 		s.AddRoutes(pathStyle, "/{bucket:.+}", "/{bucket:.+}/{key:.+}")
-		pathStyle.HandleFunc("/", s.ListBuckets).Methods(http.MethodGet)
+		// pathStyle.HandleFunc("/", s.ListBuckets).Methods(http.MethodGet)
 
 		virtualHostStyle := r.Host("{bucket:.+}." + domainName).Subrouter()
 		s.AddRoutes(virtualHostStyle, "/", "/{key:.+}")