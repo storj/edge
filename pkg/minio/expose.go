3da830e897ff420ab17833d6686b203bea4b6579
Bill Thorp <billt@storj.io>
2021-05-11 12:01:28 -0400

    gateway-mt: expose Minio internals using go:linkname workaround
    Traditionally, Gateway-MT relied on changing Minio's internals to
    be able to access its router and authorization logic.  As much as
    possible, we'd like to move that into Gateway-MT (at least for a while),
    so that it makes it easier to for Gateway-MT and Gateway-ST to both use
    the same version of Minio.
    
    This change exposes or duplicates some of these Minio internals.
    
    Change-Id: If7f4f24df9158ca08a8fc47362c750258952bb04

--- pkg/minio/expose.go
+++ pkg/minio/expose.go
@@ -1,4 +1,22 @@
-// Copyright (C) 2021 Storj Labs, Inc.
+// Copyright (c) 2015-2021 MinIO, Inc.
+//
+// This file is part of MinIO Object Storage stack
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// This code is a derivative work.
+// Derived changes Copyright (C) 2021 Storj Labs, Inc.
 // See LICENSE for copying information.
 
 package minio
@@ -9,6 +27,7 @@ import (
 	"net/url"
 	_ "unsafe" // for go:linkname
 
+	"github.com/gorilla/mux"
 	"github.com/storj/minio/cmd"
 )
 
@@ -17,6 +36,12 @@ import (
 //nolint: golint
 //go:linkname WriteErrorResponse github.com/storj/minio/cmd.writeErrorResponse
 func WriteErrorResponse(ctx context.Context, w http.ResponseWriter, err cmd.APIError, reqURL *url.URL)
+
+//  WriteErrorResponseString exposes minio's cmd.writeErrorResponseString
+//
+//nolint: golint
+//go:linkname WriteErrorResponseString github.com/storj/minio/cmd.writeErrorResponseString
+func WriteErrorResponseString(ctx context.Context, w http.ResponseWriter, err cmd.APIError, reqURL *url.URL)
 
 // GetAPIError exposes minio's cmd.getAPIError
 //
@@ -29,3 +54,52 @@ func GetAPIError(code cmd.APIErrorCode) cmd.APIError
 //nolint: golint
 //go:linkname ToAPIErrorCode github.com/storj/minio/cmd.toAPIErrorCode
 func ToAPIErrorCode(ctx context.Context, err error) (apiErr cmd.APIErrorCode)
+
+// SetObjectLayer exposes minio's cmd.setObjectLayer
+//
+//nolint: golint
+//go:linkname SetObjectLayer github.com/storj/minio/cmd.setObjectLayer
+func SetObjectLayer(o cmd.ObjectLayer)
+
+// HandleCommonEnvVars exposes minio's cmd.handleCommonEnvVars
+//
+//nolint: golint
+//go:linkname HandleCommonEnvVars github.com/storj/minio/cmd.handleCommonEnvVars
+func HandleCommonEnvVars()
+
+// CorsHandler exposes minio's cmd.corsHandler
+//
+//nolint: golint
+//go:linkname CorsHandler github.com/storj/minio/cmd.corsHandler
+func CorsHandler(handler http.Handler) http.Handler
+
+// TODO: This function will be necessary when we update Minio.
+// // RejectUnsupportedAPIs exposes minio's cmd.rejectUnsupportedAPIs
+// //
+// //nolint: golint
+// //go:linkname RejectUnsupportedAPIs github.com/storj/minio/cmd.rejectUnsupportedAPIs
+// func RejectUnsupportedAPIs(router *mux.Router)
+
+// GlobalHandlers exposes minio's cmd.globalHandlers
+//
+//nolint: golint
+//go:linkname GlobalHandlers github.com/storj/minio/cmd.globalHandlers
+var GlobalHandlers []mux.MiddlewareFunc
+
+// GlobalCLIContext exposes minio's cmd.globalCLIContext
+//
+//nolint: golint
+//go:linkname GlobalCLIContext github.com/storj/minio/cmd.globalCLIContext
+var GlobalCLIContext = struct {
+	JSON           bool
+	Quiet          bool
+	Anonymous      bool
+	Addr           string
+	StrictS3Compat bool
+}{}
+
+// GlobalIAMSys exposes minio's cmd.globalIAMSys
+//
+//nolint: golint
+//go:linkname GlobalIAMSys github.com/storj/minio/cmd.globalIAMSys
+var GlobalIAMSys *cmd.IAMSys
---
+++ pkg/minio/util.go
@@ -0,0 +1,83 @@
+// Copyright (c) 2015-2021 MinIO, Inc.
+//
+// This file is part of MinIO Object Storage stack
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// This code is a derivative work.
+// Derived changes Copyright (C) 2021 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package minio
+
+import (
+	"net/http"
+	"reflect"
+	"unsafe"
+	_ "unsafe" // for go:linkname
+
+	"github.com/storj/minio/cmd"
+	"github.com/storj/minio/cmd/logger"
+)
+
+// TODO: Revisit this when we update Minio.
+// https://github.com/storj/minio/commit/04e6dc87c349a1c51f80fbe3f60eb1716ab349e9
+
+// SetGlobalCLI offers an alternative to handleCommonCmdArgs to set global CLI context options.
+// This is needed by Storj to (at least) ensure that ETags are not random.
+func SetGlobalCLI(json, quiet, anonymous bool, addr string, strictS3Compat bool) {
+	GlobalCLIContext.JSON = json
+	GlobalCLIContext.Quiet = quiet
+	GlobalCLIContext.Anonymous = anonymous
+	GlobalCLIContext.Addr = addr
+	GlobalCLIContext.StrictS3Compat = strictS3Compat
+}
+
+// InitCustomStore initializes an IAM store, shortcutting much of minio's startup.
+func InitCustomStore(store cmd.IAMStorageAPI, sysType cmd.UsersSysType) {
+	iamSys := cmd.NewIAMSys()
+	rs := reflect.ValueOf(iamSys).Elem()
+	setUnexportedField(rs.Field(8), store)
+	setUnexportedField(rs.Field(1), sysType)
+	GlobalIAMSys = iamSys
+}
+
+func setUnexportedField(field reflect.Value, value interface{}) {
+	reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem().Set(reflect.ValueOf(value))
+}
+
+// ProjectUsageLimit storage Satellite project usage limit.
+type ProjectUsageLimit cmd.GenericError
+
+func (e ProjectUsageLimit) Error() string {
+	return "You have reached your Storj project upload limit on the Satellite."
+}
+
+// CriticalErrorHandler handles critical server failures caused by
+// `panic(logger.ErrCritical)` as done by `logger.CriticalIf`.
+//
+// It should be always the first / highest HTTP handler.
+type CriticalErrorHandler struct{ Handler http.Handler }
+
+func (h CriticalErrorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if err := recover(); err == logger.ErrCritical { // handle
+			WriteErrorResponse(r.Context(), w, GetAPIError(cmd.ErrInternalError), r.URL)
+			return
+		} else if err != nil {
+			panic(err) // forward other panic calls
+		}
+	}()
+	h.Handler.ServeHTTP(w, r)
+}
