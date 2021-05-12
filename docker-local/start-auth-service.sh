b5af60e31818a0128809231998006cfb2e8ede83
Sean Harvey <sean@storj.io>
2021-05-12 16:33:08 +1200

    gateway-mt: fix docker-local setup
    ensure correct satellite id is passed to authservice, and remove
    `debug.control` flag to gateway-mt which no longer exists.
    
    Change-Id: Ibf29d473b455285fe359783f4cec6c4f70391c64

--- docker-local/start-auth-service.sh
+++ docker-local/start-auth-service.sh
@@ -7,4 +7,12 @@ export PATH=$GOPATH/bin:$PATH
 cd /code
 go install -v storj.io/gateway-mt/cmd/authservice
 
-authservice run --auth-token "super-secret" --allowed-satellites="storjsim:10000" --endpoint="http://localhost:7777"
+until storj-sim network env --config-dir /config/local-network &>/dev/null; do
+    echo "storj-sim not ready, sleeping"
+    sleep 3
+done
+
+SATELLITE_ID="$(storj-sim network env SATELLITE_0_ID --config-dir /config/local-network)"
+echo "Found satellite ID: $SATELLITE_ID"
+
+authservice run --auth-token="super-secret" --allowed-satellites="${SATELLITE_ID}@" --endpoint="http://localhost:7777"
