b5305270bf1e85322dda7f9df0fcc6fa3b5f2664
Artur M. Wolff <artur@storj.io>
2021-07-22 12:24:41 +0200

    cmd: add a tool to remove unused records
    This change adds a tool to remove unused records from the auth service
    database. The tool is intended to be run by the prod owner.
    
    Change-Id: I63a6e5c6101a77d26a75ddd7f93c02cbed067361

---
+++ cmd/authservice-unused-records/main.go
@@ -0,0 +1,236 @@
+// Copyright (C) 2021 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package main
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/spacemonkeygo/monkit/v3"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/zeebo/errs"
+	"go.uber.org/zap"
+
+	"storj.io/gateway-mt/auth/sqlauth"
+	"storj.io/private/dbutil"
+	"storj.io/private/dbutil/pgutil"
+	"storj.io/private/process"
+	"storj.io/private/tagsql"
+)
+
+var mon = monkit.Package()
+
+var (
+	rootCmd   = &cobra.Command{Use: "authservice-unused-records"}
+	deleteCmd = &cobra.Command{Use: "delete", RunE: deleteCommand}
+	config    Config
+)
+
+// Config holds flags' values.
+type Config struct {
+	AuthServiceDB      string
+	AsOfSystemInterval time.Duration
+	MacaroonHead       []byte
+	SelectSize         int
+	DeleteSize         int
+	DryRun             bool
+}
+
+func (config *Config) bindFlags(set *pflag.FlagSet) {
+	set.StringVar(&config.AuthServiceDB, "authservicedb", "", "DSN for the auth service's database")
+	set.DurationVar(&config.AsOfSystemInterval, "as-of-system-interval", -5*time.Second, "the interval specified in AS OF SYSTEM")
+	set.BytesHexVar(&config.MacaroonHead, "macaroon-head", []byte{}, "macaroon head")
+	set.IntVar(&config.SelectSize, "select-size", 10000, "batch size of records selected for deletion at a time")
+	set.IntVar(&config.DeleteSize, "delete-size", 1000, "batch size of records to delete from selected records at a time")
+	set.BoolVar(&config.DryRun, "dry-run", false, "specifying this flag will only calculate the number of records to delete and will not delete any")
+}
+
+// VerifyFlags verifies whether flags have correct values and reports any error
+// encountered.
+func (config *Config) VerifyFlags() error {
+	var errlist errs.Group
+
+	if config.AuthServiceDB == "" {
+		errlist.Add(errors.New("authservicedb must be set"))
+	}
+	if config.AsOfSystemInterval > -time.Microsecond {
+		errlist.Add(errors.New("as-of-system-interval cannot be greater than -1 μs"))
+	}
+	if len(config.MacaroonHead) == 0 {
+		errlist.Add(errors.New("macaroon-head must be set"))
+	}
+	if config.SelectSize < 1 || config.SelectSize > 20000 {
+		errlist.Add(errors.New("select-size must be 0 < select-size < 20000"))
+	}
+	if config.DeleteSize < 1 || config.DeleteSize > 5000 {
+		errlist.Add(errors.New("delete-size must be 0 < delete-size < 5000"))
+	}
+
+	return errlist.Err()
+}
+
+func init() {
+	rootCmd.AddCommand(deleteCmd)
+
+	config.bindFlags(deleteCmd.Flags())
+}
+
+func main() {
+	process.Exec(rootCmd)
+}
+
+func deleteCommand(cmd *cobra.Command, _ []string) error {
+	if err := config.VerifyFlags(); err != nil {
+		return err
+	}
+
+	ctx, _ := process.Ctx(cmd)
+
+	log := zap.L()
+
+	opts := sqlauth.Options{
+		ApplicationName: "authservice-unused-records",
+	}
+
+	k, err := sqlauth.Open(ctx, log, config.AuthServiceDB, opts)
+	if err != nil {
+		return err
+	}
+
+	defer func() { err = errs.Combine(err, k.Close()) }()
+
+	db, impl := k.TestingTagSQL(), dbutil.ImplementationForScheme(k.TestingSchema())
+
+	wouldDelete, count, rounds, err := Delete(ctx, log, db, impl, config)
+
+	if config.DryRun {
+		log.Sugar().Infof("Would delete %d records", wouldDelete)
+	} else {
+		log.Info("Stats", zap.Int64("count", count), zap.Int64("rounds", rounds))
+	}
+
+	return err
+}
+
+// Delete performs deletion and returns the number of records it would delete
+// (for the `--dry-run` flag), number of deleted records, number of rounds it
+// took to delete these records, and any error encountered.
+func Delete(ctx context.Context, log *zap.Logger, db tagsql.DB, impl dbutil.Implementation, cfg Config) (wouldDelete int, count, rounds int64, err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	var marker [32]byte
+
+	for {
+		pkvals, err := selectUnused(ctx, db, impl, cfg.AsOfSystemInterval, cfg.MacaroonHead, marker[:], cfg.SelectSize)
+		if err != nil {
+			return wouldDelete, count, rounds, err
+		}
+
+		wouldDelete += len(pkvals)
+
+		if len(pkvals) == 0 {
+			return wouldDelete, count, rounds, nil
+		}
+
+		copy(marker[:], pkvals[len(pkvals)-1])
+
+		for !cfg.DryRun && len(pkvals) > 0 {
+			var batch [][]byte
+
+			batch, pkvals = batchValues(pkvals, cfg.DeleteSize)
+
+			res, err := db.ExecContext(
+				ctx,
+				`
+				DELETE
+				FROM records
+				WHERE encryption_key_hash = ANY ($1::BYTEA[])
+				`,
+				pgutil.ByteaArray(batch),
+			)
+			if err != nil {
+				return wouldDelete, count, rounds, err
+			}
+
+			c, err := res.RowsAffected()
+			if err == nil { // Not every database or database driver may support RowsAffected.
+				count += c
+			}
+
+			rounds++
+		}
+
+		if !cfg.DryRun && impl == dbutil.Cockroach {
+			time.Sleep(-cfg.AsOfSystemInterval)
+		}
+	}
+}
+
+func selectUnused(ctx context.Context, db tagsql.DB, impl dbutil.Implementation, asOfSystemInterval time.Duration, macaroonHead, marker []byte, selectSize int) (pkvals [][]byte, err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// We don't commit the transaction and only roll it back, in any case, to
+	// release any read locks early.
+	defer func() { err = errs.Combine(err, tx.Rollback()) }()
+
+	if impl == dbutil.Cockroach {
+		if _, err = tx.ExecContext(
+			ctx,
+			"SET TRANSACTION"+impl.AsOfSystemInterval(asOfSystemInterval),
+		); err != nil {
+			return nil, err
+		}
+	}
+
+	rows, err := tx.QueryContext(
+		ctx,
+		`
+		SELECT encryption_key_hash
+		FROM records
+		WHERE expires_at IS NULL
+		  AND macaroon_head = $1::BYTEA
+		  AND encryption_key_hash > $2::BYTEA
+		ORDER BY encryption_key_hash
+		LIMIT $3
+		`,
+		macaroonHead,
+		marker,
+		selectSize,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() { err = errs.Combine(err, rows.Close()) }()
+
+	for rows.Next() {
+		var pkval []byte
+
+		if err = rows.Scan(&pkval); err != nil {
+			return nil, err
+		}
+
+		pkvals = append(pkvals, pkval)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return pkvals, nil
+}
+
+func batchValues(pkvals [][]byte, threshold int) ([][]byte, [][]byte) {
+	if len(pkvals) < threshold {
+		return pkvals, nil
+	}
+	return pkvals[:threshold], pkvals[threshold:]
+}
---
+++ cmd/authservice-unused-records/main_test.go
@@ -0,0 +1,219 @@
+// Copyright (C) 2021 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package main_test
+
+import (
+	"context"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+
+	"storj.io/common/testcontext"
+	"storj.io/gateway-mt/auth"
+	"storj.io/gateway-mt/auth/sqlauth"
+	cmd "storj.io/gateway-mt/cmd/authservice-unused-records"
+	"storj.io/private/dbutil"
+	"storj.io/private/dbutil/pgtest"
+	"storj.io/private/dbutil/tempdb"
+)
+
+func TestVerifyFlags(t *testing.T) {
+	for _, tt := range []struct {
+		config  cmd.Config
+		wantErr bool
+	}{
+		{
+			config:  cmd.Config{},
+			wantErr: true,
+		},
+		{
+			config: cmd.Config{
+				AuthServiceDB:      "",
+				AsOfSystemInterval: 0,
+				MacaroonHead:       make([]byte, 0),
+				SelectSize:         0,
+				DeleteSize:         5001,
+				DryRun:             false,
+			},
+			wantErr: true,
+		},
+		{
+			config: cmd.Config{
+				AuthServiceDB:      "test",
+				AsOfSystemInterval: -5 * time.Second,
+				MacaroonHead:       []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 100, 255},
+				SelectSize:         12345,
+				DeleteSize:         1234,
+				DryRun:             true,
+			},
+			wantErr: false,
+		},
+	} {
+		assert.Equal(t, tt.wantErr, tt.config.VerifyFlags() != nil)
+	}
+}
+
+func TestDelete_Postgres(t *testing.T) {
+	testDelete(t, pgtest.PickPostgres(t), 0)
+}
+
+func TestDelete_Cockroach(t *testing.T) {
+	testDelete(t, pgtest.PickCockroachAlt(t), 100*time.Millisecond)
+}
+
+func testDelete(t *testing.T, connstr string, wait time.Duration) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	kv, err := openTest(ctx, zap.NewNop(), t.Name(), connstr)
+	require.NoError(t, err)
+	defer func() { require.NoError(t, kv.Close()) }()
+
+	require.NoError(t, kv.Ping(ctx))
+	require.NoError(t, kv.MigrateToLatest(ctx))
+
+	config := cmd.Config{
+		AuthServiceDB:      connstr,
+		AsOfSystemInterval: -wait,
+		MacaroonHead:       []byte{1, 3, 3, 7},
+		SelectSize:         200,
+		DeleteSize:         50,
+		DryRun:             true,
+	}
+
+	n := time.Now().Round(time.Second)
+
+	r0 := &auth.Record{
+		SatelliteAddress:     "abc",
+		MacaroonHead:         []byte{0},
+		EncryptedSecretKey:   []byte{1},
+		EncryptedAccessGrant: []byte{2},
+	}
+	r1 := &auth.Record{
+		SatelliteAddress:     "def",
+		MacaroonHead:         config.MacaroonHead,
+		EncryptedSecretKey:   []byte{1},
+		EncryptedAccessGrant: []byte{2},
+	}
+	r2 := &auth.Record{
+		SatelliteAddress:     "ghi",
+		MacaroonHead:         config.MacaroonHead,
+		EncryptedSecretKey:   []byte{1},
+		EncryptedAccessGrant: []byte{2},
+		ExpiresAt:            &n,
+	}
+
+	const iterations = 10000
+
+	for i := 0; i < iterations; i++ {
+		switch i % 3 {
+		case 0:
+			require.NoError(t, kv.Put(ctx, itob32(i), r0))
+		case 1:
+			require.NoError(t, kv.Put(ctx, itob32(i), r1))
+		case 2:
+			require.NoError(t, kv.Put(ctx, itob32(i), r2))
+		}
+	}
+
+	time.Sleep(wait)
+
+	db, impl := kv.TestingTagSQL(), dbutil.ImplementationForScheme(kv.TestingSchema())
+	wouldDelete, count, rounds, err := cmd.Delete(ctx, zap.NewNop(), db, impl, config)
+
+	require.NoError(t, err)
+	assert.Equal(t, 3333, wouldDelete)
+	assert.Equal(t, int64(0), count)
+	assert.Equal(t, int64(0), rounds)
+
+	// Confirm `--dry-run` does not delete any data:
+
+	time.Sleep(wait)
+
+	for i := 0; i < iterations; i++ {
+		switch i % 3 {
+		case 0:
+			r, err := kv.GetWithNonDefaultAsOfInterval(ctx, itob32(i), -wait)
+			require.NoError(t, err)
+			assert.Equal(t, r0, r)
+		case 1:
+			r, err := kv.GetWithNonDefaultAsOfInterval(ctx, itob32(i), -wait)
+			require.NoError(t, err)
+			assert.Equal(t, r1, r)
+		case 2:
+			r, err := kv.GetWithNonDefaultAsOfInterval(ctx, itob32(i), -wait)
+			require.NoError(t, err)
+			assert.Equal(t, r2, r)
+		}
+	}
+
+	// Perform deletion:
+
+	config.DryRun = false
+
+	wouldDelete, count, rounds, err = cmd.Delete(ctx, zap.NewNop(), db, impl, config)
+
+	require.NoError(t, err)
+	assert.Equal(t, 3333, wouldDelete)
+	assert.Equal(t, int64(3333), count)
+	assert.Equal(t, int64(67), rounds)
+
+	// Confirm deletion deleted only relevant data:
+
+	time.Sleep(wait)
+
+	for i := 0; i < iterations; i++ {
+		switch i % 3 {
+		case 0:
+			r, err := kv.GetWithNonDefaultAsOfInterval(ctx, itob32(i), -wait)
+			require.NoError(t, err)
+			assert.Equal(t, r0, r)
+		case 1:
+			r, err := kv.GetWithNonDefaultAsOfInterval(ctx, itob32(i), -wait)
+			require.NoError(t, err)
+			assert.Nil(t, r)
+		case 2:
+			r, err := kv.GetWithNonDefaultAsOfInterval(ctx, itob32(i), -wait)
+			require.NoError(t, err)
+			assert.Equal(t, r2, r)
+		}
+	}
+}
+
+func openTest(ctx context.Context, log *zap.Logger, name, connstr string) (*sqlauth.KV, error) {
+	tempDB, err := tempdb.OpenUnique(ctx, connstr, name)
+	if err != nil {
+		return nil, err
+	}
+
+	opts := sqlauth.Options{
+		ApplicationName: "authservice-unused-records_test",
+	}
+
+	kv, err := sqlauth.Open(ctx, log, tempDB.ConnStr, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	kv.TestingSetCleanup(tempDB.Close)
+
+	return kv, nil
+}
+
+func itob32(i int) [32]byte {
+	var k [32]byte
+
+	for j, r := range strconv.Itoa(i) {
+		if j > 31 {
+			panic("out of range")
+		}
+		k[j] = byte(r)
+	}
+
+	return k
+}
