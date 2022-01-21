d3e4bf0ff0cbfb8caed706afa2a9e5c7240715a3
Bill Thorp <billt@storj.io>
2022-01-21 15:38:59 -0500

    pkg/server/middleware:  add concurrency limiter
    Add a concurrency limiter that prevents multiple concurrent requests.
    
    Using this limiter will be part of a later commit.
    
    Updates storj/gateway-mt#131
    
    Change-Id: I152445e3216f0c29bbffc62a2d19f3648c33a05b

---
+++ pkg/server/middleware/limiter.go
@@ -0,0 +1,82 @@
+// Copyright (C) 2022 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package middleware
+
+import (
+	"net/http"
+	"sync"
+
+	"storj.io/common/grant"
+)
+
+// NewMacaroonLimiter constructs a Limiter that limits based on macaroon credentials.
+// It relies on the AccessKey middleware being run to append credentials to the request context.
+func NewMacaroonLimiter(allowed uint, errFunc, limitFunc func(w http.ResponseWriter, r *http.Request)) *Limiter {
+	return NewLimiter(allowed, getRequestMacaroonHead, errFunc, limitFunc)
+}
+
+// getRequestMacaroonHead gets the macaroon head corresponding to the current request.
+// Macaroon head is the best available criteria for associating a request to a user.
+func getRequestMacaroonHead(r *http.Request) (ip string, err error) {
+	credentials := GetAccess(r.Context())
+	if credentials == nil || credentials.AccessGrant == "" {
+		return "", ParseV4CredentialError.New("missing access grant")
+	}
+	access, err := grant.ParseAccess(credentials.AccessGrant)
+	if err != nil {
+		return "", err
+	}
+	return string(access.APIKey.Head()), err
+}
+
+// Limiter imposes a limit per key.
+type Limiter struct {
+	allowed   uint // maximum concurrent allowed
+	keyFunc   func(*http.Request) (string, error)
+	errFunc   func(w http.ResponseWriter, r *http.Request)
+	limitFunc func(w http.ResponseWriter, r *http.Request)
+
+	limits map[string]uint
+	m      sync.RWMutex
+}
+
+// NewLimiter constructs a concurrency Limiter.  Error and Limit functions are user defined
+// in part because referencing the "minio" package here would cause an import loop.
+func NewLimiter(allowed uint, keyFunc func(*http.Request) (string, error), errFunc, limitFunc func(w http.ResponseWriter, r *http.Request)) *Limiter {
+	return &Limiter{
+		allowed:   allowed,
+		limits:    make(map[string]uint),
+		keyFunc:   keyFunc,
+		errFunc:   errFunc,
+		limitFunc: limitFunc,
+	}
+}
+
+// Limit applies per-key request concurrency limiting as an HTTP middleware.
+func (l *Limiter) Limit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		key, err := l.keyFunc(r)
+		if err != nil {
+			l.errFunc(w, r)
+			return
+		}
+		l.m.Lock()
+		l.limits[key]++
+		l.m.Unlock()
+		l.m.RLock()
+		if l.limits[key] > l.allowed {
+			l.m.RUnlock()
+			l.limitFunc(w, r)
+		} else {
+			l.m.RUnlock()
+			next.ServeHTTP(w, r)
+		}
+		l.m.Lock()
+		l.limits[key]--
+		if l.limits[key] == 0 {
+			delete(l.limits, key)
+		}
+		l.m.Unlock()
+	})
+}
---
+++ pkg/server/middleware/limiter_test.go
@@ -0,0 +1,151 @@
+// Copyright (C) 2022 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
+
+	"storj.io/common/grant"
+	"storj.io/common/macaroon"
+	"storj.io/common/testcontext"
+	"storj.io/gateway-mt/pkg/authclient"
+)
+
+const maxConncurrent = 3
+
+func TestNewMacaroonLimiter(t *testing.T) {
+	var next = make(chan struct{})
+	var done = make(chan struct{}, maxConncurrent*2*3)
+	var allTests = make(chan struct{})
+	rateLimiter := NewMacaroonLimiter(maxConncurrent,
+		func(w http.ResponseWriter, r *http.Request) {
+			next <- struct{}{} // create in-order results
+			http.Error(w, "", http.StatusInternalServerError)
+		},
+		func(w http.ResponseWriter, r *http.Request) {
+			next <- struct{}{} // create in-order results
+			http.Error(w, "", http.StatusTooManyRequests)
+		},
+	)
+
+	// run ratelimiter cleanup until end of test
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
+	// make the default HTTP handler return StatusOK after waiting
+	handler := rateLimiter.Limit(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		next <- struct{}{} // create in-order results
+		<-allTests
+	}))
+
+	// expect burst number of successes
+	creds1 := getCredentials(t)
+	testWithMacaroon(ctx, t, creds1, handler, next, done)
+	// expect similar results for a different apiKey / macaroon
+	creds2 := getCredentials(t)
+	testWithMacaroon(ctx, t, creds2, handler, next, done)
+	// expect similar results for a different apiKey / macaroon
+	creds3 := getCredentials(t)
+	testWithMacaroon(ctx, t, creds3, handler, next, done)
+	close(allTests)
+
+	// wait for previous responses to arrive so we can retest cred1
+	for x := 0; x < maxConncurrent*2*3; x++ {
+		<-done
+	}
+	// expect burst number of successes with cred1 again
+	allTests = make(chan struct{})
+	testWithMacaroon(ctx, t, creds1, handler, next, done)
+	close(allTests)
+}
+
+func testWithMacaroon(ctx *testcontext.Context, t *testing.T, creds *Credentials, handler http.Handler, next, done chan struct{}) {
+	// expect maxConncurrent HTTP 200s, then maxConncurrent HTTP 429s
+	for x := 0; x < maxConncurrent*2; x++ {
+		localX := x
+		ctx.Go(func() error {
+			responseCode := doRequest(ctx, t, creds, handler)
+			if localX < maxConncurrent {
+				assert.Equal(t, http.StatusOK, responseCode, localX)
+			} else {
+				assert.Equal(t, http.StatusTooManyRequests, responseCode, localX)
+			}
+			done <- struct{}{}
+			return nil
+		})
+		<-next // wait until concurrency middleware has recorded the previous request
+	}
+}
+
+func doRequest(ctx context.Context, t *testing.T, creds *Credentials, handler http.Handler) int {
+	credCtx := context.WithValue(ctx, credentialsCV{}, creds)
+	req, err := http.NewRequestWithContext(credCtx, "GET", "", nil)
+	require.NoError(t, err)
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+	return rr.Code
+}
+
+func getCredentials(t *testing.T) *Credentials {
+	apiKey, err := macaroon.NewAPIKey([]byte("secret"))
+	require.NoError(t, err)
+	ag := grant.Access{
+		SatelliteAddress: "satelliteAddress",
+		APIKey:           apiKey,
+		EncAccess:        grant.NewEncryptionAccess(),
+	}
+	grant, err := ag.Serialize()
+	require.NoError(t, err)
+	return &Credentials{
+		AccessKey: "accessKey",
+		AuthServiceResponse: authclient.AuthServiceResponse{
+			AccessGrant: grant,
+			SecretKey:   "secretKey",
+			Public:      true,
+		},
+		Error: nil,
+	}
+}
+
+func simpleKeyFunc(r *http.Request) (string, error) {
+	return strconv.Itoa(r.ProtoMajor), nil
+}
+func noopHandler(w http.ResponseWriter, r *http.Request) {}
+
+// How quickly can we go through 10 clients with 100 requests, with 10 request
+// limits for each?
+func benchmarkLimiter(l *Limiter) error {
+	var g errgroup.Group
+	for i := 0; i < 10; i++ {
+		j := i
+		g.Go(func() error {
+			r := &http.Request{ProtoMajor: j}
+			var g2 errgroup.Group
+			for k := 0; k < 100; k++ {
+				g2.Go(func() error {
+					l.Limit(http.HandlerFunc(noopHandler)).ServeHTTP(nil, r)
+					return nil
+				})
+			}
+			return g2.Wait()
+		})
+	}
+	return g.Wait()
+}
+func BenchmarkLimiter(b *testing.B) {
+	l := NewLimiter(10, simpleKeyFunc, noopHandler, noopHandler)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		require.NoError(b, benchmarkLimiter(l))
+	}
+}
