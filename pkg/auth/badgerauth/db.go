storj/edge

commit 652b63d457632b58524ed2853e1196bae544d4b0

pkg/auth/badgerauth: silence ErrNoRewrite in gcValueLog
This change modifies gcValueLog so that it won't alarm typical users
that something's wrong when ErrNoRewrite is actually expected (other
codebases using BadgerDB also silence ErrNoRewrite for this purpose).

Change-Id: I742edea063439f194b061b82e8ed8c6dd492b39c

pkg/auth/badgerauth/db.go
@@ -126,7 +126,10 @@ func (db *DB) gcValueLog(ctx context.Context) (err error) {
 		case <-ctx.Done():
 			err = ctx.Err()
 		default:
-			err = db.db.RunValueLogGC(0.5)
+			// Run GC and optionally silence ErrNoRewrite errors:
+			if err = db.db.RunValueLogGC(.5); errs.Is(err, badger.ErrNoRewrite) {
+				err = nil
+			}
 		}
 		gcFinished(&err)
 	}

commit 1e85c3131b2e45600cb91c11403cd554f936acc7

pkg/auth/badgerauth: fix infinite loop in gcValueLog
652b63d introduced an infinite loop in gcValueLog. This change fixes it
by adding a necessary additional condition to make sure the loop always
exits.

Change-Id: I98d41e72eb899062775f9e46b7c68e3743e5c169

pkg/auth/badgerauth/db.go
@@ -120,6 +120,7 @@ func OpenDB(log *zap.Logger, config Config) (*DB, error) {
 func (db *DB) gcValueLog(ctx context.Context) (err error) {
 	defer mon.Task()(&ctx)(nil)
 
+gcLoop:
 	for err == nil {
 		gcFinished := mon.TaskNamed("gc")(&ctx)
 		select {
@@ -128,7 +129,9 @@ func (db *DB) gcValueLog(ctx context.Context) (err error) {
 		default:
 			// Run GC and optionally silence ErrNoRewrite errors:
 			if err = db.db.RunValueLogGC(.5); errs.Is(err, badger.ErrNoRewrite) {
+				gcFinished(nil)
 				err = nil
+				break gcLoop
 			}
 		}
 		gcFinished(&err)