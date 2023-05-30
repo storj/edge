f48e6d552b2ba0252c6d28b993c0cc5df34e8adf
Sean Harvey <sean@storj.io>
2023-05-30 14:49:52 +1200

    cmd/authservice-admin: add links commands to inspect and revoke links
    This adds the following new commands to authservice-admin:
    * links inspect
    * links revoke
    
    These commands can be used to get details on one or more links, without
    the need to manually check each one. It can also be used to revoke
    access to one or more links at once.
    
    Additionally, this also changes the existing record commands to
    optionally get more details on the API key from the satellite admin if
    it's configured. If it's not configured, it just uses the authservice
    nodes only.
    
    It also adjusts the "tabbed" output of record commands to be consistent
    with these new links commands and list out each field on a new line, as
    the existing tabbedwriter output was becoming very difficult to read
    with so many columns.
    
    Updates #321
    
    Change-Id: I9d0bc1c73c389384e05e729eac3449de665292af

---
+++ cmd/authservice-admin/links.go
@@ -0,0 +1,259 @@
+// Copyright (C) 2023 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/zeebo/clingy"
+	"github.com/zeebo/errs"
+	"golang.org/x/sync/errgroup"
+
+	"storj.io/common/storj"
+	"storj.io/gateway-mt/internal/authadminclient"
+	"storj.io/gateway-mt/internal/satelliteadminclient"
+	"storj.io/gateway-mt/pkg/auth/authdb"
+	"storj.io/gateway-mt/pkg/sharedlink"
+)
+
+type result struct {
+	record
+	URL    string `json:"url"`
+	Online bool   `json:"online"`
+	Error  string `json:"error,omitempty"`
+}
+
+type cmdLinksInspect struct {
+	output          string
+	inputFilePath   string
+	authAdminClient *authadminclient.AuthAdminClient
+	satAdminClients map[string]*satelliteadminclient.Client
+}
+
+func (cmd *cmdLinksInspect) Setup(params clingy.Parameters) {
+	cmd.authAdminClient = newAuthAdminClient(params)
+	cmd.satAdminClients = mustSatAdminClients(params)
+	cmd.output = params.Flag("output", "output format (either json or leave empty to output as text)", "", clingy.Short('o')).(string)
+	cmd.inputFilePath = params.Arg("input", "input file path").(string)
+}
+
+func (cmd *cmdLinksInspect) Execute(ctx context.Context) (err error) {
+	urls, err := scanURLs(cmd.inputFilePath)
+	if err != nil {
+		return err
+	}
+
+	results := make([]result, len(urls))
+
+	for i, url := range urls {
+		results[i].URL = url
+
+		online, err := isOnline(ctx, url)
+		if err != nil {
+			results[i].Error = err.Error()
+			continue
+		}
+		results[i].Online = online
+
+		if err := cmd.setRecords(ctx, url, &results[i]); err != nil {
+			results[i].Error = err.Error()
+		}
+	}
+
+	switch cmd.output {
+	case "json":
+		return json.NewEncoder(os.Stdout).Encode(&results)
+	default:
+		printResults(results)
+		return nil
+	}
+}
+
+func (cmd *cmdLinksInspect) setRecords(ctx context.Context, url string, result *result) error {
+	link, err := sharedlink.Parse(url)
+	if err != nil {
+		return err
+	}
+
+	authRecord, err := cmd.authAdminClient.Resolve(ctx, link.AccessKey)
+	if err != nil {
+		return err
+	}
+
+	result.AuthRecord = authRecord
+
+	if cmd.satAdminClients != nil {
+		satelliteNodeURL, err := storj.ParseNodeURL(authRecord.SatelliteAddress)
+		if err != nil {
+			return err
+		}
+
+		satAdminClient, ok := cmd.satAdminClients[satelliteNodeURL.Address]
+		if !ok {
+			return errs.New("could not find satellite admin address for %q", satelliteNodeURL.Address)
+		}
+
+		apiKeyResp, err := satAdminClient.GetAPIKey(ctx, authRecord.APIKey)
+		if err != nil {
+			return err
+		}
+
+		result.SatRecord = apiKeyResp
+	}
+
+	return nil
+}
+
+// isOnline checks multiple HTTP methods to see if the link appears online.
+//
+// We consider a link is online if any one of HEAD or GET requests returns a
+// good response. We do this because S3 presigned links are signed for a
+// particular method only, so we can't always do a HEAD request reliably.
+func isOnline(ctx context.Context, url string) (bool, error) {
+	methods := []string{http.MethodHead, http.MethodGet}
+	responses := make([]bool, len(methods))
+	var group errgroup.Group
+	for i, method := range methods {
+		i := i
+		method := method
+		group.Go(func() error {
+			req, err := http.NewRequestWithContext(ctx, method, url, nil)
+			if err != nil {
+				return err
+			}
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				return err
+			}
+			_ = resp.Body.Close()
+
+			switch {
+			case resp.StatusCode == http.StatusOK:
+				responses[i] = true
+			case resp.StatusCode >= 500:
+				return errs.New("unexpected status: %s", resp.Status)
+			default:
+				responses[i] = false
+			}
+
+			return nil
+		})
+	}
+	if err := group.Wait(); err != nil {
+		return false, err
+	}
+	for _, online := range responses {
+		if online {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func printResults(results []result) {
+	for _, r := range results {
+		fmt.Println(r.URL)
+		fmt.Println("---")
+		printFixed("Online:", strconv.FormatBool(r.Online))
+		if r.Error != "" {
+			printFixed("Error:", r.Error)
+		}
+		printRecord(r.record)
+		fmt.Println("")
+	}
+}
+
+type cmdLinksRevoke struct {
+	inputFilePath   string
+	authAdminClient *authadminclient.AuthAdminClient
+	satAdminClients map[string]*satelliteadminclient.Client
+}
+
+func (cmd *cmdLinksRevoke) Setup(params clingy.Parameters) {
+	cmd.authAdminClient = newAuthAdminClient(params)
+	cmd.satAdminClients = mustSatAdminClients(params)
+	cmd.inputFilePath = params.Arg("input", "input file path").(string)
+}
+
+func (cmd *cmdLinksRevoke) Execute(ctx context.Context) (err error) {
+	urls, err := scanURLs(cmd.inputFilePath)
+	if err != nil {
+		return err
+	}
+
+	var errGroup errs.Group
+	for _, url := range urls {
+		if err := cmd.revokeURL(ctx, url); err != nil {
+			errGroup.Add(errs.New("revoke url %q: %w", url, err))
+		}
+	}
+
+	return errGroup.Err()
+}
+
+func (cmd *cmdLinksRevoke) revokeURL(ctx context.Context, url string) error {
+	link, err := sharedlink.Parse(url)
+	if err != nil {
+		return err
+	}
+
+	authRecord, err := cmd.authAdminClient.Resolve(ctx, link.AccessKey)
+	if err != nil {
+		return err
+	}
+
+	satelliteNodeURL, err := storj.ParseNodeURL(authRecord.SatelliteAddress)
+	if err != nil {
+		return err
+	}
+
+	satAdminClient, ok := cmd.satAdminClients[satelliteNodeURL.Address]
+	if !ok {
+		return errs.New("could not find satellite admin address for %q", satelliteNodeURL.Address)
+	}
+
+	if err := satAdminClient.DeleteAPIKey(ctx, authRecord.APIKey); err != nil {
+		return err
+	}
+
+	// note: accessKey could be an access grant, so we need to check first.
+	if len(link.AccessKey) == authdb.EncKeySizeEncoded {
+		if err := cmd.authAdminClient.Delete(ctx, link.AccessKey); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func scanURLs(name string) ([]string, error) {
+	file, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = file.Close()
+	}()
+
+	var urls []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if strings.TrimSpace(scanner.Text()) != "" {
+			urls = append(urls, scanner.Text())
+		}
+	}
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+
+	return urls, nil
+}
--- cmd/authservice-admin/main.go
+++ cmd/authservice-admin/main.go
@@ -5,19 +5,17 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"strconv"
 	"strings"
-	"text/tabwriter"
-	"time"
 
 	"github.com/zeebo/clingy"
 
-	client "storj.io/gateway-mt/internal/authadminclient"
+	"storj.io/gateway-mt/internal/authadminclient"
+	"storj.io/gateway-mt/internal/satelliteadminclient"
 )
 
 var logger *log.Logger
@@ -36,10 +34,14 @@ func main() {
 		}
 
 		cmds.Group("record", "record commands", func() {
-			cmds.New("show", "show a record", new(cmdShow))
-			cmds.New("invalidate", "invalidate a record", new(cmdInvalidate))
-			cmds.New("unpublish", "unpublish a record", new(cmdUnpublish))
-			cmds.New("delete", "delete a record", new(cmdDelete))
+			cmds.New("show", "show a record", new(cmdRecordShow))
+			cmds.New("invalidate", "invalidate a record", new(cmdRecordInvalidate))
+			cmds.New("unpublish", "unpublish a record", new(cmdRecordUnpublish))
+			cmds.New("delete", "delete a record", new(cmdRecordDelete))
+		})
+		cmds.Group("links", "links commands", func() {
+			cmds.New("inspect", "inspect given links and return a report", new(cmdLinksInspect))
+			cmds.New("revoke", "revoke access for given links", new(cmdLinksRevoke))
 		})
 	})
 	if err != nil {
@@ -50,129 +52,41 @@ func main() {
 	}
 }
 
-type cmdShow struct {
-	clientConfig client.Config
-	key          string
-	output       string
-	expanded     bool
+func newAuthAdminClient(params clingy.Parameters) *authadminclient.AuthAdminClient {
+	return authadminclient.New(authadminclient.Config{
+		NodeAddresses: params.Flag("node-addresses", "comma delimited list of authservice node addresses", []string{},
+			clingy.Transform(func(s string) ([]string, error) {
+				return strings.Split(s, ","), nil
+			})).([]string),
+		CertsDir: params.Flag("certs-dir", "directory of certificates for authentication with authservice nodes", "").(string),
+	}, logger)
 }
 
-func (cmd *cmdShow) Setup(params clingy.Parameters) {
-	setupClientConfig(params, &cmd.clientConfig)
-
-	cmd.output = params.Flag("output", "output format (valid options: tabbed, json)", "tabbed",
-		clingy.Short('o'),
-	).(string)
-	cmd.expanded = params.Flag("expanded", "show more record information", false,
-		clingy.Short('x'),
-		clingy.Transform(strconv.ParseBool), clingy.Boolean,
-	).(bool)
-	cmd.key = params.Arg("key", "Access key ID or key hash").(string)
-}
-
-func (cmd *cmdShow) Execute(ctx context.Context) error {
-	record, err := client.New(cmd.clientConfig, logger).Get(ctx, cmd.key)
-	if err != nil {
-		return err
-	}
-
-	switch cmd.output {
-	case "tabbed", "":
-		return printTabbedRecord(record, cmd.expanded)
-	case "json":
-		return json.NewEncoder(os.Stdout).Encode(record)
-	default:
-		return fmt.Errorf("unsupported output %q (valid options: tabbed, json)", cmd.output)
-	}
-}
-
-type cmdInvalidate struct {
-	clientConfig client.Config
-	key          string
-	reason       string
-}
-
-func (cmd *cmdInvalidate) Setup(params clingy.Parameters) {
-	setupClientConfig(params, &cmd.clientConfig)
-
-	cmd.key = params.Arg("key", "Access key ID or key hash").(string)
-	cmd.reason = params.Arg("reason", "invalidation reason").(string)
-}
-
-func (cmd *cmdInvalidate) Execute(ctx context.Context) error {
-	return client.New(cmd.clientConfig, logger).Invalidate(ctx, cmd.key, cmd.reason)
-}
-
-type cmdUnpublish struct {
-	clientConfig client.Config
-	key          string
-}
-
-func (cmd *cmdUnpublish) Setup(params clingy.Parameters) {
-	setupClientConfig(params, &cmd.clientConfig)
-
-	cmd.key = params.Arg("key", "Access key ID or key hash").(string)
-}
-
-func (cmd *cmdUnpublish) Execute(ctx context.Context) error {
-	return client.New(cmd.clientConfig, logger).Unpublish(ctx, cmd.key)
-}
-
-type cmdDelete struct {
-	clientConfig client.Config
-	key          string
-}
-
-func (cmd *cmdDelete) Setup(params clingy.Parameters) {
-	setupClientConfig(params, &cmd.clientConfig)
-
-	cmd.key = params.Arg("key", "Access key ID or key hash").(string)
-}
-
-func (cmd *cmdDelete) Execute(ctx context.Context) error {
-	return client.New(cmd.clientConfig, logger).Delete(ctx, cmd.key)
-}
-
-func setupClientConfig(params clingy.Parameters, config *client.Config) {
-	config.NodeAddresses = params.Flag("node-addresses", "comma delimited list of node addresses", []string{},
+func mustSatAdminClients(params clingy.Parameters) map[string]*satelliteadminclient.Client {
+	values := params.Flag("satellite-admin-addresses", "comma delimited list of satellite admin addresses, e.g. us1.storj.io:7777=http://localhost:10005=123,eu1.storj.io:7777=http://localhost:10006=456", []string{},
 		clingy.Transform(func(s string) ([]string, error) {
 			return strings.Split(s, ","), nil
 		})).([]string)
-	config.CertsDir = params.Flag("certs-dir", "directory of certificates for authentication", "").(string)
-	config.InsecureDisableTLS = params.Flag("insecure-disable-tls", "disable tls for testing", false,
-		clingy.Transform(strconv.ParseBool), clingy.Boolean,
-	).(bool)
+
+	if len(values) == 0 {
+		return nil
+	}
+
+	clients := make(map[string]*satelliteadminclient.Client)
+	if err := loadSatAdminClients(clients, values); err != nil {
+		panic(err)
+	}
+
+	return clients
 }
 
-func printTabbedRecord(r *client.Record, expanded bool) error {
-	w := tabwriter.NewWriter(os.Stdout, 2, 2, 2, ' ', 0)
-	headers := []string{"CREATED", "PUBLIC"}
-	values := []string{time.Unix(r.CreatedAtUnix, 0).UTC().Format(time.RFC3339), strconv.FormatBool(r.Public)}
-	if r.ExpiresAtUnix != 0 {
-		headers = append(headers, "EXPIRES")
-		values = append(values, time.Unix(r.ExpiresAtUnix, 0).UTC().Format(time.RFC3339))
-	}
-	if r.InvalidatedAtUnix != 0 {
-		headers = append(headers, "INVALIDATED")
-		values = append(values, time.Unix(r.InvalidatedAtUnix, 0).UTC().Format(time.RFC3339))
-	}
-	if r.InvalidationReason != "" {
-		headers = append(headers, "INVALIDATION REASON")
-		values = append(values, r.InvalidationReason)
-	}
-	if expanded {
-		headers = append(headers, "SATELLITE", "MACAROON HEAD")
-		values = append(values, r.SatelliteAddress, r.MacaroonHeadHex)
-		if r.APIKey != "" {
-			headers = append(headers, "API KEY")
-			values = append(values, r.APIKey)
+func loadSatAdminClients(clients map[string]*satelliteadminclient.Client, values []string) error {
+	for _, value := range values {
+		parts := strings.Split(value, "=")
+		if len(parts) != 3 {
+			return fmt.Errorf("invalid satellite mapping %q", value)
 		}
-		if r.DecryptedAccessGrant != "" {
-			headers = append(headers, "ACCESS GRANT")
-			values = append(values, r.DecryptedAccessGrant)
-		}
+		clients[parts[0]] = satelliteadminclient.New(parts[1], parts[2])
 	}
-	fmt.Fprintln(w, strings.Join(headers, "\t"))
-	fmt.Fprintln(w, strings.Join(values, "\t"))
-	return w.Flush()
+	return nil
 }
