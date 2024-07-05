4a0abe7972f66115f0492fb57b411043fe5bbd26
Artur M. Wolff <artur@storj.io>
2024-07-05 09:31:20 +0200

    pkg/accesslogs: small fixes to S3-style entries
    goos: linux
    goarch: amd64
    pkg: storj.io/edge/pkg/accesslogs
    cpu: AMD Ryzen 5 1600 Six-Core Processor
                                │   old.txt   │               new.txt               │
                                │   sec/op    │   sec/op     vs base                │
    S3AccessLogEntryToString-12   5.396µ ± 3%   3.077µ ± 5%  -42.98% (p=0.000 n=10)
    
    Change-Id: I380d3b3b667273e26dbd6f58cb96de23cbc7d983

--- pkg/accesslogs/s3.go
+++ pkg/accesslogs/s3.go
@@ -3,59 +3,152 @@
 package accesslogs
 
 import (
-	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"storj.io/common/memory"
 )
 
-// S3AccessLogEntry represents all the fields that are needed for producing S3 access log entry and implements the Entry interface.
-type S3AccessLogEntry struct {
-	logline string
-
-	BucketOwner        string        // example: 79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be, should be project UUID of the project for us
+// S3AccessLogEntryOptions represents all fields needed for producing
+// S3-style server access log entry.
+type S3AccessLogEntryOptions struct {
+	BucketOwner        string        // example: 79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be
 	Bucket             string        // example: DOC-EXAMPLE-BUCKET1
-	Timestamp          time.Time     // example: 06/Feb/2019:00:00:38 +0000
-	RemoteIp           string        // example: 192.0.2.3
-	Requester          string        // example: 79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be, should be hardcoded '' for iuse
-	RequestId          string        // example: 3E57427F3EXAMPLE
-	Operation          string        // example: REST.GET.VERSIONING
+	Time               time.Time     // example: [06/Feb/2019:00:00:38 +0000]
+	RemoteIP           string        // example: 192.0.2.3
+	Requester          string        // example: 79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be
+	RequestID          string        // example: 3E57427F33A59F07
+	Operation          string        // example: REST.PUT.OBJECT
 	Key                string        // example: /photos/2019/08/puppy.jpg
-	RequestURI         string        // example: GET /DOC-EXAMPLE-BUCKET1/photos/2019/08/puppy.jpg?x-foo=bar HTTP/1.1, it's OK if we include credentials
-	HttpStatus         int64         // example: 200
+	RequestURI         string        // example: "GET /DOC-EXAMPLE-BUCKET1/photos/2019/08/puppy.jpg?x-foo=bar HTTP/1.1"
+	HTTPStatus         int           // example: 200
 	ErrorCode          string        // example: NoSuchBucket
-	BytesSent          int64         // example: 2662992
-	ObjectSize         int64         // example: 3462992
+	BytesSent          int           // example: 2662992
+	ObjectSize         *int          // example: 3462992
 	TotalTime          time.Duration // example: 70
-	TurnAroundTime     time.Duration // example: 10, this can be fetched from Metrics Middleware probably?
-	Referer            string        // example: http://www.example.com/webservices
-	UserAgent          string        // example: curl/7.15.1
-	VersionId          string        // example: 3HL4kqtJvjVBH40Nrjfkd, check both request and response headers in search for versionId
-	HostId             string        // example: SHA for gateway's hostname, e.g. for dp-prod-edge-ch1-2
-	SignatureVersion   string        // example: SigV4, todo: most of the requests are SigV4 anyways, but let's get the actual value
-	CipherSuite        string        // example: ECDHE-RSA-AES128-GCM-SHA256 r.TLS.CipherSuite or - if it doesn't exist, 	// if r.TLS != nil
-	AuthenticationType string        // example: AuthHeader, same as signature version. Get it from credentials parser.
+	TurnAroundTime     time.Duration // example: 10
+	Referer            string        // example: "http://www.example.com/webservices"
+	UserAgent          string        // example: "curl/7.15.1"
+	VersionID          string        // example: 3HL4kqtJvjVBH40Nrjfkd
+	HostID             string        // example: s9lzHYrFp76ZVxRcpX9+5cjAnEH2ROuNkd2BHfIa6UkFVdtjf5mKR3/eTPFvsiP/XV/VLi31234=
+	SignatureVersion   string        // example: SigV2
+	CipherSuite        string        // example: ECDHE-RSA-AES128-GCM-SHA256
+	AuthenticationType string        // example: AuthHeader
 	HostHeader         string        // example: s3.us-west-2.amazonaws.com
-	TlsVersion         string        // example: TLSv1.2, get it from r.TLS
-	AccessPointARN     string        // example: arn:aws:s3:us-east-1:123456789012:accesspoint/example-AP, we don't use ARNs so let's '-' it
-	AclRequired        string        // example: Yes
+	TLSVersion         string        // example: TLSv1.2
+	AccessPointARN     string        // example: arn:aws:s3:us-east-1:123456789012:accesspoint/example-AP
+	ACLRequired        string        // example: Yes
 }
 
-// Size returns the size of the Entry in bytes, so it meets the Entry interface.
-func (a S3AccessLogEntry) Size() memory.Size {
-	return memory.Size(len(a.logline))
+// S3AccessLogEntry represents the S3-style server access log entry.
+type S3AccessLogEntry struct {
+	b *strings.Builder
 }
 
-// String returns the formatted S3 access log entry (as per https://docs.aws.amazon.com/AmazonS3/latest/userguide/LogFormat.html), so it meets the Entry interface.
-func (a S3AccessLogEntry) String() string {
-	return fmt.Sprintf("%s %s [%s] %s %s %s %s %s \"%s\" %d %s %d %d %d %d \"%s\" \"%s\" %s %s %s %s %s %s %s %s %s\n",
-		a.BucketOwner, a.Bucket, S3formattedstrftime(a.Timestamp), a.RemoteIp, a.Requester, a.RequestId, a.Operation, a.Key, a.RequestURI, a.HttpStatus, a.ErrorCode, a.BytesSent, a.ObjectSize,
-		a.TotalTime/time.Millisecond, a.TurnAroundTime/time.Millisecond, a.Referer, a.UserAgent, a.VersionId, a.HostId, a.SignatureVersion, a.CipherSuite, a.AuthenticationType, a.HostHeader,
-		a.TlsVersion, a.AccessPointARN, a.AclRequired)
+// NewS3AccessLogEntry creates new S3AccessLogEntry.
+//
+// It assumes that all relevant fields are already escaped.
+func NewS3AccessLogEntry(o S3AccessLogEntryOptions) *S3AccessLogEntry {
+	e := new(S3AccessLogEntry)
+	e.b = new(strings.Builder)
+
+	e.writeString(o.BucketOwner)
+	e.writeString(o.Bucket)
+
+	if !o.Time.IsZero() {
+		e.b.WriteRune('[')
+		e.b.WriteString(o.Time.UTC().Format("02/Jan/2006:15:04:05 -0700"))
+		e.b.WriteRune(']')
+	} else {
+		e.b.WriteRune('-')
+	}
+	e.b.WriteRune(' ')
+
+	e.writeString(o.RemoteIP)
+	e.writeString(o.Requester)
+	e.writeString(o.RequestID)
+	e.writeString(o.Operation)
+	e.writeString(o.Key)
+	e.writeQuotedString(o.RequestURI)
+	e.writeInt(o.HTTPStatus)
+	e.writeString(o.ErrorCode)
+	e.writeInt(o.BytesSent)
+
+	if o.ObjectSize != nil {
+		e.b.WriteString(strconv.Itoa(*o.ObjectSize))
+	} else {
+		e.b.WriteRune('-')
+	}
+	e.b.WriteRune(' ')
+
+	e.writeInt64(o.TotalTime.Milliseconds())
+	e.writeInt64(o.TurnAroundTime.Milliseconds())
+	e.writeQuotedString(o.Referer)
+	e.writeQuotedString(o.UserAgent)
+	e.writeString(o.VersionID)
+	e.writeString(o.HostID)
+	e.writeString(o.SignatureVersion)
+	e.writeString(o.CipherSuite)
+	e.writeString(o.AuthenticationType)
+	e.writeString(o.HostHeader)
+	e.writeString(o.TLSVersion)
+	e.writeString(o.AccessPointARN)
+
+	if o.ACLRequired != "" {
+		e.b.WriteString(o.ACLRequired)
+	} else {
+		e.b.WriteRune('-')
+	}
+
+	return e
 }
 
-// S3formattedstrftime formats the time in the way S3 expects it: [%d/%b/%Y:%H:%M:%S %z].
-func S3formattedstrftime(t time.Time) string {
-	t = t.UTC()
-	return t.Format("02/Jan/2006:15:04:05 -0700")
+// Size returns the size of the entry.
+func (e S3AccessLogEntry) Size() memory.Size {
+	return memory.Size(e.b.Len())
+}
+
+// String returns the formatted entry (as per
+// https://docs.aws.amazon.com/AmazonS3/latest/userguide/LogFormat.html).
+func (e S3AccessLogEntry) String() string {
+	return e.b.String()
+}
+
+func (e S3AccessLogEntry) writeString(s string) {
+	if s != "" {
+		e.b.WriteString(s)
+	} else {
+		e.b.WriteRune('-')
+	}
+	e.b.WriteRune(' ')
+}
+
+func (e S3AccessLogEntry) writeQuotedString(s string) {
+	if s != "" {
+		e.b.WriteRune('"')
+		e.b.WriteString(s)
+		e.b.WriteRune('"')
+	} else {
+		e.b.WriteRune('-')
+	}
+	e.b.WriteRune(' ')
+}
+
+func (e S3AccessLogEntry) writeInt(i int) {
+	if i > 0 {
+		e.b.WriteString(strconv.Itoa(i))
+	} else {
+		e.b.WriteRune('-')
+	}
+	e.b.WriteRune(' ')
+}
+
+func (e S3AccessLogEntry) writeInt64(i int64) {
+	if i > 0 {
+		e.b.WriteString(strconv.FormatInt(i, 10))
+	} else {
+		e.b.WriteRune('-')
+	}
+	e.b.WriteRune(' ')
 }
--- pkg/accesslogs/s3_test.go
+++ pkg/accesslogs/s3_test.go
@@ -5,91 +5,88 @@ package accesslogs
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
-// TestHelloName calls greetings.Hello with a name, checking
-// for a valid return value.
-func TestS3AccessLogEntryToString(t *testing.T) {
-	const layout = "2006-01-02T15:04:05"
+func TestS3AccessLogEntry(t *testing.T) {
+	testTime, err := time.Parse(time.RFC3339, "2024-12-21T13:45:10Z")
+	require.NoError(t, err)
 
-	// Calling Parse() method with its parameters
-	randomTime, _ := time.Parse(layout, "2024-12-21T13:45:10")
-	ale := S3AccessLogEntry{
+	objectSize := 0
+
+	o := S3AccessLogEntryOptions{
 		BucketOwner:        "bucketOwner",
 		Bucket:             "bucketName",
-		Timestamp:          randomTime,
-		RemoteIp:           "8.8.8.8",
+		Time:               testTime,
+		RemoteIP:           "8.8.8.8",
 		Requester:          "publicProjectId",
-		RequestId:          "requestId",
+		RequestID:          "requestId",
 		Operation:          "REST.GET.VERSIONING",
 		Key:                "/test/puppy.jpg",
 		RequestURI:         "GET /DOC-EXAMPLE-BUCKET1/photos/2019/08/puppy.jpg?x-foo=bar HTTP/1.1",
-		HttpStatus:         200,
+		HTTPStatus:         200,
 		ErrorCode:          "NoSuchBucket",
 		BytesSent:          1234,
-		ObjectSize:         0,
+		ObjectSize:         &objectSize,
 		TotalTime:          123 * time.Millisecond,
-		TurnAroundTime:     0,
+		TurnAroundTime:     456 * time.Millisecond,
 		Referer:            "https://example.com",
 		UserAgent:          "curl 8.8",
-		VersionId:          "versionId",
-		HostId:             "hostId",
+		VersionID:          "versionId",
+		HostID:             "hostId",
 		SignatureVersion:   "SigV4",
 		CipherSuite:        "TLS_AES_128_GCM_SHA256",
 		AuthenticationType: "AuthHeader",
 		HostHeader:         "gateway.storjshare.io",
-		TlsVersion:         "TLSv1.2",
-		AccessPointARN:     "-",
-		AclRequired:        "-",
+		TLSVersion:         "TLSv1.2",
 	}
 
-	msg := ale.String()
-	want := "bucketOwner bucketName [21/Dec/2024:13:45:10 +0000] 8.8.8.8 publicProjectId requestId REST.GET.VERSIONING /test/puppy.jpg \"GET /DOC-EXAMPLE-BUCKET1/photos/2019/08/puppy.jpg?x-foo=bar HTTP/1.1\" 200 NoSuchBucket 1234 0 123 0 \"https://example.com\" \"curl 8.8\" versionId hostId SigV4 TLS_AES_128_GCM_SHA256 AuthHeader gateway.storjshare.io TLSv1.2 - -\n"
-	if msg != want {
-		t.Fatalf("Unexpected S3-formatting: %q, want: %q", msg, want)
-	}
+	e := NewS3AccessLogEntry(o)
+
+	require.True(t, e.Size() > 0)
+
+	want := "bucketOwner bucketName [21/Dec/2024:13:45:10 +0000] 8.8.8.8 publicProjectId requestId REST.GET.VERSIONING /test/puppy.jpg \"GET /DOC-EXAMPLE-BUCKET1/photos/2019/08/puppy.jpg?x-foo=bar HTTP/1.1\" 200 NoSuchBucket 1234 0 123 456 \"https://example.com\" \"curl 8.8\" versionId hostId SigV4 TLS_AES_128_GCM_SHA256 AuthHeader gateway.storjshare.io TLSv1.2 - -"
+	require.Equal(t, want, e.String())
 }
 
-var resultLogline string
+var result string
 
 func BenchmarkS3AccessLogEntryToString(b *testing.B) {
-	const layout = "2006-01-02T15:04:05"
-	randomTime, _ := time.Parse(layout, "2024-12-21T13:45:10")
-	ale := S3AccessLogEntry{
+	testTime, err := time.Parse(time.RFC3339, "2024-12-21T13:45:10Z")
+	require.NoError(b, err)
+
+	o := S3AccessLogEntryOptions{
 		BucketOwner:        "bucketOwner",
 		Bucket:             "bucketName",
-		Timestamp:          randomTime,
-		RemoteIp:           "8.8.8.8",
+		Time:               testTime,
+		RemoteIP:           "8.8.8.8",
 		Requester:          "publicProjectId",
-		RequestId:          "requestId",
+		RequestID:          "requestId",
 		Operation:          "REST.GET.VERSIONING",
 		Key:                "/test/puppy.jpg",
 		RequestURI:         "GET /DOC-EXAMPLE-BUCKET1/photos/2019/08/puppy.jpg?x-foo=bar HTTP/1.1",
-		HttpStatus:         200,
+		HTTPStatus:         200,
 		ErrorCode:          "NoSuchBucket",
 		BytesSent:          1234,
-		ObjectSize:         0,
 		TotalTime:          123 * time.Millisecond,
-		TurnAroundTime:     0,
+		TurnAroundTime:     456 * time.Millisecond,
 		Referer:            "https://example.com",
 		UserAgent:          "curl 8.8",
-		VersionId:          "versionId",
-		HostId:             "hostId",
+		VersionID:          "versionId",
+		HostID:             "hostId",
 		SignatureVersion:   "SigV4",
 		CipherSuite:        "TLS_AES_128_GCM_SHA256",
 		AuthenticationType: "AuthHeader",
 		HostHeader:         "gateway.storjshare.io",
-		TlsVersion:         "TLSv1.2",
-		AccessPointARN:     "-",
-		AclRequired:        "-",
+		TLSVersion:         "TLSv1.2",
 	}
 
 	b.ResetTimer()
 
-	var logline string
+	var r string
 	for i := 0; i < b.N; i++ {
-		logline = ale.String()
+		r = NewS3AccessLogEntry(o).String()
 	}
-	// Store the result to a package level variable, so the compiler cannot eliminate the Benchmark itself? https://dave.cheney.net/2013/06/30/how-to-write-benchmarks-in-go
-	resultLogline = logline
+	result = r
 }
