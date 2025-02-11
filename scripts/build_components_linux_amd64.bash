54c02e69920b79cd509138c560825b54e4f4ea90
Sean Harvey <sean@storj.io>
2025-02-12 11:34:25 +1300

    {Makefile,scripts}: fix building binaries on branch
    Makefile expects "release/${TAG}" is where the binaries will be
    placed, but the script doesn't take building from a branch into
    account. This adds another argument to specify the output rather
    than the script trying to guess where it should go instead.
    
    Change-Id: I2382f8d56865ebc5f173145e2342297e715ebbb8

--- scripts/build_components_linux_amd64.bash
+++ scripts/build_components_linux_amd64.bash
@@ -3,13 +3,12 @@ set -euxo pipefail
 
 COMPONENTS=${1}
 GO_VERSION=${2}
+OUT=${3}
 
 GOGO_VERSION=go${GO_VERSION}
 
-COM=$(git rev-parse --short HEAD)
-OUT="release/${COM}"
-TAG=$(git describe --tags --exact-match --match v[0-9]*.[0-9]*.[0-9]*) && OUT="${OUT}-${TAG}" || TAG="v0.0.0"
-OUT="${OUT}-${GOGO_VERSION}"
+REVISION=$(git rev-parse --short HEAD)
+VERSION=$(git describe --tags --exact-match --match v[0-9]*.[0-9]*.[0-9]*) || VERSION="v0.0.0"
 DIR=$(mktemp -d)
 
 wget -P ${DIR} https://github.com/amwolff/go/archive/refs/tags/${GOGO_VERSION}.tar.gz
@@ -23,7 +22,7 @@ for C in ${COMPONENTS//,/ }; do
     for GOARCH in arm arm64 amd64; do
         CGO_ENABLED=0 GOOS=linux GOARCH=${GOARCH} ${DIR}/${GOGO_VERSION}/bin/go build \
             -o ${OUT}/${C}_linux_${GOARCH} \
-            -ldflags "-X storj.io/common/version.buildTimestamp=$(date +%s) -X storj.io/common/version.buildCommitHash=${COM} -X storj.io/common/version.buildVersion=${TAG} -X storj.io/common/version.buildRelease=true" \
+            -ldflags "-X storj.io/common/version.buildTimestamp=$(date +%s) -X storj.io/common/version.buildCommitHash=${REVISION} -X storj.io/common/version.buildVersion=${VERSION} -X storj.io/common/version.buildRelease=true" \
             ./cmd/${C}
     done
 done
