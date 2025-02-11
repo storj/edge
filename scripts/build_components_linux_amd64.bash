83dad50ed68145f5a0206e4908ce393c3ed27dfd
Sean Harvey <sean@storj.io>
2025-02-12 12:00:59 +1300

    scripts: fix shellcheck complaints in build script
    Change-Id: Iaed87f68a29e6ae2701cbc6306dff0135824a07f

--- scripts/build_components_linux_amd64.bash
+++ scripts/build_components_linux_amd64.bash
@@ -11,18 +11,18 @@ REVISION=$(git rev-parse --short HEAD)
 VERSION=$(git describe --tags --exact-match --match v[0-9]*.[0-9]*.[0-9]*) || VERSION="v0.0.0"
 DIR=$(mktemp -d)
 
-wget -P ${DIR} https://github.com/amwolff/go/archive/refs/tags/${GOGO_VERSION}.tar.gz
-mkdir ${DIR}/${GOGO_VERSION}
-tar -xzf ${DIR}/${GOGO_VERSION}.tar.gz --strip-components=1 -C ${DIR}/${GOGO_VERSION}
-cd ${DIR}/${GOGO_VERSION}/src
+wget -P "${DIR}" "https://github.com/amwolff/go/archive/refs/tags/${GOGO_VERSION}.tar.gz"
+mkdir "${DIR}/${GOGO_VERSION}"
+tar -xzf "${DIR}/${GOGO_VERSION}.tar.gz" --strip-components=1 -C "${DIR}/${GOGO_VERSION}"
+cd "${DIR}/${GOGO_VERSION}/src"
 ./make.bash
 cd -
 
 for C in ${COMPONENTS//,/ }; do
-    for GOARCH in arm arm64 amd64; do
-        CGO_ENABLED=0 GOOS=linux GOARCH=${GOARCH} ${DIR}/${GOGO_VERSION}/bin/go build \
-            -o ${OUT}/${C}_linux_${GOARCH} \
+    for ARCH in arm arm64 amd64; do
+        CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} "${DIR}/${GOGO_VERSION}/bin/go" build \
+            -o "${OUT}/${C}_linux_${ARCH}" \
             -ldflags "-X storj.io/common/version.buildTimestamp=$(date +%s) -X storj.io/common/version.buildCommitHash=${REVISION} -X storj.io/common/version.buildVersion=${VERSION} -X storj.io/common/version.buildRelease=true" \
-            ./cmd/${C}
+            "./cmd/${C}"
     done
 done
